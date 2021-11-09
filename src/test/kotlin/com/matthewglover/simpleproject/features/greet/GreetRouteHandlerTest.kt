cd7b92d843ae9362515fe1544326407d62cfd19b
Matt Glover <matt.p.glover@pm.me>
2021-11-09 09:42:23 +0000

    Update test name

--- src/test/kotlin/com/matthewglover/simpleproject/features/greet/GreetRouteHandlerTest.kt
+++ src/test/kotlin/com/matthewglover/simpleproject/features/greet/GreetRouteHandlerTest.kt
@@ -5,7 +5,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 
-class GreetIntegrationTest {
+class GreetRouteHandlerTest {
 
     @Test
     fun `a valid request returns a valid greeting response`() {
