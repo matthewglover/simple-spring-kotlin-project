36a56be6b8b4670fa0ff070b6903aad8416b5d9a
Matthew Glover <matthewglover@Matthews-MacBook-Pro.local>
2022-01-06 14:30:56 +0000

    Refactor RequestDataParsingErrorResponseMapper

--- api/src/main/kotlin/com/matthewglover/simpleproject/common/errormappers/RequestDataParsingErrorResponseMapper.kt
+++ api/src/main/kotlin/com/matthewglover/simpleproject/common/errormappers/RequestDataParsingErrorResponseMapper.kt
@@ -6,60 +6,41 @@ import com.matthewglover.simpleproject.common.errors.RequestDataParsingError
 import com.matthewglover.simpleproject.common.errors.UnexpectedRefiningError
 import com.matthewglover.simpleproject.common.errors.ValidationErrors
 import com.matthewglover.simpleproject.common.errors.messages
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.bodyValueAndAwait
 
 object RequestDataParsingErrorResponseMapper {
-    suspend fun map(error: RequestDataParsingError): ServerResponse =
-        when (error) {
-            is MissingRequestPayloadError -> missingRequestPayloadError()
+
+    suspend fun map(error: RequestDataParsingError): ServerResponse {
+        val errorResponse = when (error) {
+            is MissingRequestPayloadError -> missingRequestPayloadErrorResponse()
             is JsonDecodingError -> decodingErrorResponse(error)
             is ValidationErrors -> validationErrorResponse(error)
             is UnexpectedRefiningError -> refiningErrorResponse(error)
         }
 
-    private suspend fun missingRequestPayloadError(): ServerResponse {
-        val errorResponse = ErrorResponse(
-            errorType = MissingRequestPayloadError.javaClass.simpleName,
-            errors = setOf(MissingRequestPayloadError.message)
-        )
-
         return badRequestResponse(errorResponse)
     }
 
-    private suspend fun decodingErrorResponse(error: JsonDecodingError): ServerResponse {
-        val errorResponse =
-            ErrorResponse(
-                errorType = error.javaClass.simpleName,
-                errors = setOf(error.decodingException.localizedMessage)
-            )
+    private fun missingRequestPayloadErrorResponse() = ErrorResponse(
+        errorType = MissingRequestPayloadError.javaClass.simpleName,
+        errors = setOf(MissingRequestPayloadError.message)
+    )
 
-        return badRequestResponse(errorResponse)
-    }
+    private fun decodingErrorResponse(error: JsonDecodingError) = ErrorResponse(
+        errorType = error.javaClass.simpleName,
+        errors = setOf(error.decodingException.localizedMessage)
+    )
 
-    private suspend fun validationErrorResponse(error: ValidationErrors): ServerResponse {
-        val errorResponse =
-            ErrorResponse(errorType = error.javaClass.simpleName, errors = error.messages())
+    private fun validationErrorResponse(error: ValidationErrors) =
+        ErrorResponse(errorType = error.javaClass.simpleName, errors = error.messages())
 
-        return badRequestResponse(errorResponse)
-    }
+    private fun refiningErrorResponse(error: UnexpectedRefiningError) =
+        ErrorResponse(errorType = error.javaClass.simpleName, errors = setOf(error.throwable.localizedMessage))
 
-    private suspend fun refiningErrorResponse(error: UnexpectedRefiningError): ServerResponse {
-        val errorResponse =
-            ErrorResponse(errorType = error.javaClass.simpleName, errors = setOf(error.throwable.localizedMessage))
-
-        return internalServerErrorResponse(errorResponse)
-    }
-
-    private suspend fun <T : Any> badRequestResponse(t: T) =
+    private suspend fun badRequestResponse(errorResponse: ErrorResponse) =
         ServerResponse.badRequest()
             .contentType(MediaType.APPLICATION_JSON)
-            .bodyValueAndAwait(t)
-
-    private suspend fun <T : Any> internalServerErrorResponse(t: T) =
-        ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .contentType(MediaType.APPLICATION_JSON)
-            .bodyValueAndAwait(t)
+            .bodyValueAndAwait(errorResponse)
 }
