73a86a8b6d3bd21f37c65ce022e11baf2afe47a6
Matthew Glover <matthewglover@Matthews-MacBook-Pro.local>
2022-01-06 12:01:30 +0000

    Fix Open API annotations

--- api/src/main/kotlin/com/matthewglover/simpleproject/features/users/UserRouteConfig.kt
+++ api/src/main/kotlin/com/matthewglover/simpleproject/features/users/UserRouteConfig.kt
@@ -2,7 +2,11 @@ package com.matthewglover.simpleproject.features.users
 
 import com.matthewglover.simpleproject.common.routeutils.RouteUtils
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
 import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.parameters.RequestBody
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springdoc.core.annotations.RouterOperation
 import org.springdoc.core.annotations.RouterOperations
@@ -14,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod
 @Configuration
 class UserRouteConfig {
 
-    @Bean
     @RouterOperations(
         value = [
             RouterOperation(
@@ -25,6 +28,7 @@ class UserRouteConfig {
                 operation = Operation(
                     operationId = "get-user",
                     summary = "Get a user by userId",
+                    parameters = [Parameter(name = "userId", `in` = ParameterIn.PATH)],
                     responses = [
                         ApiResponse(
                             responseCode = "200",
@@ -41,11 +45,15 @@ class UserRouteConfig {
             RouterOperation(
                 path = "/users/",
                 method = [RequestMethod.POST],
-                beanClass = UserService::class,
-                beanMethod = "addUser",
+                beanClass = UserHandlers::class,
+                beanMethod = "handleAddUser",
                 operation = Operation(
                     operationId = "add-user",
                     summary = "Add a new user",
+                    method = "POST",
+                    requestBody = RequestBody(
+                        content = [Content(schema = Schema(implementation = RefinedNewUser::class))]
+                    ),
                     responses = [
                         ApiResponse(
                             responseCode = "200",
@@ -64,6 +72,7 @@ class UserRouteConfig {
             )
         ]
     )
+    @Bean
     fun userRoutes(routeUtils: RouteUtils, userHandlers: UserHandlers) = routeUtils.coRouter {
         GET("/users/{userId}", userHandlers::handleGetUser)
 
